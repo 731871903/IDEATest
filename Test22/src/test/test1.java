b20e01bf5cfe41b0d366e3b997f46c4d976e2b31
Leon <731871903@qq.com>
2020-10-01 11:48:09 +0800

    This is my first test of IDEA in Git

---
+++ Test22/src/test/test1.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author Liweicheng
+ * @create 2020/10/1 - 11:44
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("人间失格");
+    }
+}
